edfa0b1ac252be1b08a98e69b73f43262a8eb568
moi.rodriguez <moi.rodriguez@duocuc.cl>
2025-10-25 22:25:33 -0300

    Parte 3 - Implementación de la Interfaz Adaptativa
    Creación de screen home con imagen

---
+++ app/src/main/java/com/example/app_perfulandia/ui/screens/HomeScreenCompacta().kt
@@ -0,0 +1,59 @@
+package com.example.app_perfulandia.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.app_perfulandia.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenCompacta() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Mi App Kotlin") })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            Text(
+                text = "¡Bienvenido!",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Button(onClick = { /* acción futura */ }) {
+                Text("Presióname")
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo App",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
+}
